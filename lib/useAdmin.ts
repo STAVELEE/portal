d9622beb82a591d635e3eca3d77e6d705fd91c4e
Stave <lee@stave.kr>
2025-05-04 19:57:16 +0900

    path

--- lib/useAdmin.ts
+++ lib/useAdmin.ts
@@ -5,8 +5,9 @@ export default function useAdmin(): boolean {
   const [isAdmin, setIsAdmin] = useState(false)
 
   useEffect(() => {
-    if (typeof document !== 'undefined') {
-      setIsAdmin(document.cookie.includes('admin=true'))
+    if (typeof window !== 'undefined') {
+      const hasCookie = document.cookie.includes('admin=true')
+      setIsAdmin(hasCookie)
     }
   }, [])
 
