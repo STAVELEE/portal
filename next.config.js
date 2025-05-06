753a99caaadbcc7374c05ac6dd6a8c537a280583
Stave <lee@stave.kr>
2025-05-06 13:09:29 +0000

    update

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-  }
-  
-  module.exports = {
-    webpack: (config) => {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        crypto: require.resolve('crypto-browserify'),
-        buffer: require.resolve('buffer'),
-      }
-      return config
-    },
-  }
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: require.resolve('crypto-browserify'),
+      buffer: require.resolve('buffer'),
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
